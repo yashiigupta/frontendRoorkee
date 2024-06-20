675118ab380797c71eb4d9d99328d888c9acb18d
Yashi Gupta <yashi.g23csai@nst.rishihood.edu.in>
2024-06-20 19:38:10 +0530

    ui changes

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,17 +1,11 @@
-
+import styles from '../styles/tabs.module.css';
 
 export default function Tabs(props) {
   return (
-    <div className="mt-5 flex justify-center items-center mb-5 p-2">
-      <button className="text-center font-medium py-2 px-4 rounded-lg border border-gray-300 bg-white mr-2 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" onClick={() => props.setComponent("Schemes")}>
-        Schemes
-      </button>
-      <button className="text-center font-medium py-2 px-4 rounded-lg border border-gray-300 bg-white mr-2 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" onClick={() => props.setComponent("Job Openings")}>
-        Job Openings
-      </button>
-      <button className="text-center font-medium py-2 px-4 rounded-lg border border-gray-300 bg-white mr-2 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" onClick={() => props.setComponent("Scholarships")}>
-        Scholarships
-      </button>
+    <div className={styles.tabsSection}>
+      <button className={styles.tabBtn} onClick={() => props.setComponent("Schemes")}>Schemes</button>
+      <button className={styles.tabBtn} onClick={() => props.setComponent("Job Openings")}>Job Openings</button>
+      <button className={styles.tabBtn} onClick={() => props.setComponent("Scholarships")}>Scholarships</button>
     </div>
-  );
+  )
 }
